d75d6d845cad399569e99ca04daa037891e41f92
tiiita <tiiitamail@gmail.com>
2024-11-10 05:38:01 +0100

    Try to fix chromedriver freeze bug

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
-use std::{env, io::stdin, process::Command};
+use std::{env::{self, consts::OS}, io::stdin, process::Command};
 
-use ai_browser::{action_executor::{self}, chrome_setup::{self}, cmd_parser};
+use ai_browser::{
+    action_executor::{self},
+    chrome_setup::{self},
+    cmd_parser,
+};
 use dotenv::dotenv;
 use env_logger::Builder;
-use log::{error, info, LevelFilter};
+use log::{debug, error, info, LevelFilter};
 use reqwest::Client;
 use thirtyfour::{error::WebDriverError, ChromiumLikeCapabilities, DesiredCapabilities, WebDriver};
 
@@ -16,33 +20,42 @@ async fn main() -> Result<(), WebDriverError> {
     start_chrome_download(&http).await;
 
     let mut caps = DesiredCapabilities::chrome();
-    caps.set_binary(chrome_setup::get_predicted_path(false).as_str()).expect("Failed to set chrome binary path");
+    caps.set_binary(chrome_setup::get_predicted_path(false).as_str())
+        .expect("Failed to set chrome binary path");
     caps.set_no_sandbox().expect("Unable to deactivate sandbox");
     execute_chrome_driver();
 
     let driver = WebDriver::new("http://localhost:9515", caps).await?;
-    info!("Started WebDriver");
-
     info!("Running event loop, startup done!");
 
     loop {
         let mut buf = String::new();
         stdin().read_line(&mut buf).unwrap();
-        match cmd_parser::parse(buf.trim().to_string()) { 
-            Ok(action) => { 
+        match cmd_parser::parse(buf.trim().to_string()) {
+            Ok(action) => {
                 action_executor::execute(action, driver.clone()).await;
-             },
-            Err(err) => { error!("Got error: {err}") },
+            }
+            Err(err) => {
+                error!("Got error: {err}")
+            }
         }
     }
 }
 
 fn execute_chrome_driver() {
     tokio::spawn(async {
-        Command::new(chrome_setup::get_predicted_path(true))
-        .arg("--port=9515")
-        .output()
-        .expect("Failed to execute chromedriver binary");
+        let predicted_path = chrome_setup::get_predicted_path(true);
+        let cmd = if OS.eq("windows") {
+           predicted_path
+        } else { 
+            "./".to_string() + predicted_path.as_str()
+        };
+
+        Command::new(cmd)
+            .arg("--port=9515")
+            .output()
+            .expect("Failed to execute chromedriver binary");
+        debug!("Executed chromedriver binary");
     });
 }
 
@@ -57,8 +70,6 @@ async fn start_chrome_download(http: &Client) {
         info!("Found cache, skipping chrome(driver) download")
     }
 }
-
-
 
 fn init_logger() {
     Builder::new()
